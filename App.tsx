4ef7feb02ff87fd1785b8bcf97fa9dc084a8f941
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-20 18:18:17 +0530

    Styling react native apps

--- App.tsx
+++ App.tsx
@@ -4,19 +4,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>A piece of text</Text>
+        <Text style={styles.dummyText}>A piece of text</Text>
       </View>
 
-      <Text
-        style={{
-          margin: 16,
-          borderWidth: 1,
-          borderColor: '#EED3D9',
-          padding: 16,
-        }}
-      >
-        Hello World!
-      </Text>
+      <Text style={styles.dummyText}>Hello World!</Text>
       <Button title='Tap Me!' />
     </View>
   );
@@ -28,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#B5C0D0',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: '#EED3D9',
   },
 });
